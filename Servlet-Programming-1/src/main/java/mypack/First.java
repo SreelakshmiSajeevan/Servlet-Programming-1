d398b2fda3b44f9f91a2c5def93aa621710be9b2
SreelakshmiSajeevan <54982096+SreelakshmiSajeevan@users.noreply.github.com>
2022-08-16 09:45:51 +0530

    Add files via upload

---
+++ Servlet-Programming-1/src/main/java/mypack/First.java
@@ -0,0 +1,60 @@
+package mypack;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class First
+ */
+@WebServlet("/First")
+public class First extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public First() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		response.setContentType("text/html");
+		PrintWriter printWriter=response.getWriter();
+		
+		String username=request.getParameter("username");
+		String servername=request.getServerName();
+		int serverport=request.getServerPort();
+
+		printWriter.print(username+" "+servername+" "+serverport);
+		printWriter.print("<html>");
+		printWriter.print("<body>");
+		printWriter.print("<p>This is servlet</p>");
+		printWriter.print("</body>");
+		printWriter.print("</html>");
+		printWriter.close();
+
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
